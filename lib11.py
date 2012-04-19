ad38e3bcb3553229c6d71a045f22d5bffa025a49
Efrain Hdez Morales <efrahdez.92@gmail.com>
2012-04-19 16:32:15 +0100

    libreria para mostrar una lista con todos los valores enteros entre ellos.

---
+++ lib11.py
@@ -0,0 +1,6 @@
+def f11(x,y):
+    """Una lista con todos los valores enteros entre ellos (incluyendo x e y)"""
+    lista = [];
+    for i in range(x,y+1):
+        lista.append(i)
+    return lista;
