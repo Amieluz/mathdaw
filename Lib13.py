aa0294bc029b6d531ebb4a61a929bdaff19f6257
Daniel Ibanez <shearer671@hotmail.es>
2012-04-19 16:31:40 +0100

    Libreria para mostrar si los numeros son enteros

---
+++ Lib13.py
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------------
+# Autor: Jose Ibanez Luis
+# 19 de abril de 2012
+
+
+# funcion que muestra si los numeros son impares
+
+def f13(x, y):
+
+    """ funcion que recibe dos numeros enteros y muestra si los dos son impares"""
+
+    # comprobamos si los numeros introducidos son impares
+
+    if (x % 2 != 0) and (y % 2 != 0):
+
+        # retornamos valor booleano confirmando son impares
+
+        return true;
+
+
+
